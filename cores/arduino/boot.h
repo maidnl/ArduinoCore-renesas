a028d0404fac427448385004f35f0d5ca389dab0
maidnl <d.aimo@arduino.cc>
2024-01-11 17:09:56 +0100

    Moved go in bootloader function in separate core module

--- cores/arduino/boot.h
+++ cores/arduino/boot.h
@@ -1,5 +1,8 @@
 #include "Arduino.h"
 
+#ifndef BOOTLOADER_INCLUDE_H
+#define BOOTLOADER_INCLUDE_H
+
 /* Key code for writing PRCR register. */
 #define BSP_PRV_PRCR_KEY                 (0xA500U)
 #define BSP_PRV_PRCR_PRC1_UNLOCK          ((BSP_PRV_PRCR_KEY) | 0x2U)
@@ -14,11 +17,5 @@
 
 
 /* call this function when you want to force the board to go in bootloader */
-void goBootloader() {
-   R_SYSTEM->PRCR = (uint16_t) BSP_PRV_PRCR_PRC1_UNLOCK;
-   BOOT_DOUBLE_TAP_DATA = DOUBLE_TAP_MAGIC;
-   R_SYSTEM->PRCR = (uint16_t) BSP_PRV_PRCR_LOCK;
-   ((R_USB_FS0_Type*)R_USB_FS0_BASE)->SYSCFG_b.DPRPU = 0;
-   NVIC_SystemReset();
-   while (1); // WDT will fire here
-}
+void goBootloader();
+#endif
